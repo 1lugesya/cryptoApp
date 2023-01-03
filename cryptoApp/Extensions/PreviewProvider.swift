43821ff918fd0e3134a41bacb25be3db301b0902
1lugesya <sariaysegulll@gmail.com>
2023-01-04 00:37:28 +0300

    added market statistics section

--- cryptoApp/Extensions/PreviewProvider.swift
+++ cryptoApp/Extensions/PreviewProvider.swift
@@ -24,6 +24,10 @@ class DeveloperPreview {
     
     let homeVM = HomeViewModel()
     
+    let stat1 = StatisticModel(title: "Market Cap", value: "$12.5Bn" , percentageChange: 24.34)
+    let stat2 = StatisticModel(title: "Total Volume", value: "$1.231Tr")
+    let stat3 = StatisticModel(title: "Portfolio Value", value: "$50.4k", percentageChange: -12.34)
+    
     let coin = CoinModel(
         id: "bitcoin",
         symbol: "btc",
---
+++ cryptoApp/Models/StatisticModel.swift
@@ -0,0 +1,26 @@
+//
+//  StatisticModel.swift
+//  cryptoApp
+//
+//  Created by Ayşegül Sarı on 3.01.2023.
+//
+
+import Foundation
+
+
+struct StatisticModel: Identifiable {
+    
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+    
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+        
+    }
+}
+
+
