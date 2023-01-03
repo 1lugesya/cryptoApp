f0cc2926d70c8bf6cae8bf15bf516b7d1f3cd04c
1lugesya <sariaysegulll@gmail.com>
2023-01-03 23:03:00 +0300

    added filtering feature

--- cryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ cryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -25,12 +25,41 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers () {
-
+        
         dataService.$allCoins
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoin)
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func filterCoin(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+            
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText)
+        }
+        
+        
+        
+        
     }
 }
 
