e3d27d5035fa454b7267c1090a874c8eb690403f
1lugesya <sariaysegulll@gmail.com>
2023-01-05 23:51:33 +0300

    added MarketDataServices

---
+++ cryptoApp/Services/MarketDataServices.swift
@@ -0,0 +1,39 @@
+//
+//  MarketDataServices.swift
+//  cryptoApp
+//
+//  Created by Ayşegül Sarı on 5.01.2023.
+//
+
+import Foundation
+import Combine
+ 
+ 
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        
+        getData()
+    }
+    
+    private func getData() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global")
+        else {return}
+        
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+        
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+        
+    }
+    
+}
